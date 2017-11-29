4c676f2004ec372aba06dc1af0ad6193f7133778
Enver Sağnak <enver_543@hotmail.com>
2017-11-29 10:56:59 +0300

    SinavUygulamasi

---
+++ kullanicilar.js
@@ -0,0 +1,28 @@
+﻿[
+    {
+        "kullanici_adi":"te.ori",
+        "sifre": "123456",
+        "isim": "Orhan",
+        
+       "soyisim": "Aygün",
+       "profil_resmi": "orhan.jpg",
+       "d_tarihi": "01.01.1985",
+       "mezun_oldgu_okul": "Gazi Üni",
+       "fakulte": "ESEF",
+       "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+       "konular":["c#","javascript","html"]
+    },
+    {
+       "kullanici_adi": "enver.sagnak",
+       "sifre": "321987",
+       "isim": "Enver",
+
+       "soyisim": "Sağnak",
+       "profil_resmi": "resim.jpg",
+       "d_tarihi": "29.11.1990",
+       "mezun_oldgu_okul": "Çukurova Üniversitesi",
+       "fakulte": "Mühendislik-Mimarlık",
+       "ozgecmis": "Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum",
+       "konular": ["c#", "javascript", "html","T-SQL","Asp.NET MVC"]
+    }
+]
